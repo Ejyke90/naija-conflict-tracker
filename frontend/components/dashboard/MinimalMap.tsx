f5756bb17bc25f64470061c500952f056b568c4c
ejikeudeze <euworld7@gmail.com>
2026-01-30 00:03:19 -0500

    feat: Integrate Mapbox tiles for enhanced map quality
    - Update all map components to use Mapbox streets-v12 tiles
    - Replace OpenStreetMap and CARTO tiles with high-quality Mapbox tiles
    - Update environment variable with user's Mapbox access token
    - Improve map attribution to include Mapbox credits

--- frontend/components/dashboard/MinimalMap.tsx
+++ frontend/components/dashboard/MinimalMap.tsx
@@ -100,8 +100,8 @@ const MinimalMap: React.FC<MinimalMapProps> = ({ incidents = [] }) => {
         className="z-0"
       >
         <TileLayer
-          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          url={`https://api.mapbox.com/styles/v1/mapbox/streets-v12/tiles/{z}/{x}/{y}?access_token=${process.env.NEXT_PUBLIC_MAPBOX_TOKEN}`}
+          attribution='&copy; <a href="https://www.mapbox.com/about/maps/">Mapbox</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
         {/* Render dynamically passed incidents if available */}
         {incidents.length > 0 ? (
