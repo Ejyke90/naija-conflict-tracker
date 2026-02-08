4204b33f9e93272d75acc0f9fe4384a7ae027b6c
ejikeudeze <euworld7@gmail.com>
2026-02-08 18:27:32 -0500

    feat: add demo analytics page for testing

---
+++ frontend/src/pages/demo-analytics.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import StateAnalysis from '../components/dashboard/StateAnalysis';
+
+export default function DemoAnalytics() {
+  return (
+    <div className="min-h-screen bg-gray-50 p-8">
+      <StateAnalysis />
+    </div>
+  );
+}
